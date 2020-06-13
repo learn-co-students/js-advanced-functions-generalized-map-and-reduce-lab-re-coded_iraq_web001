bb68e7ddee361a892fa7cb1427bde8a4419d5864
Revan Sarbast <revansarbast8@gmail.com>
2020-06-13 10:45:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(src,a) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(a(src[i])) // Unique work
+  }
+  return r
+}
+function reduce(src,fn, arr = 0){
+   let val = arr
+   for(let element of src){
+     val = fn(val, element )
+   }
+   return val
+}
+sourceArray = [1, 2, true, "razmatazz"]
+console.log(reduce(sourceArray, function(a, memo){ return !!a && !!memo},true))

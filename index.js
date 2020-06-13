6c5849354efde00a864f161c61146d5571d71bae
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-13 11:23:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(sourceArr, callback){
+  let newArr = [];
+  for (let i = 0; i < sourceArr.length; i++) {
+    newArr.push(callback(sourceArr[i]));
+  }
+  return newArr;
+}
+
+function reduce(sourceArr, callback, startingValue){
+  let memo = startingValue ? startingValue : sourceArr[0];
+  for (let i = startingValue ? 0 : 1; i < sourceArr.length; i++) {
+    memo = callback(sourceArr[i], memo);
+  }
+  return memo;
+}

63477af66dac2e3db5911ead59cfb4484e27bf4b
zeena kareem <zena34269@gmail.com>
2020-06-16 13:04:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+function map(array,callback){
+  let newArray =[]
+  for(let i=0;i<array.length;i++){
+   newArray.push(callback(array[i]))
+  }
+  return newArray
+}
+function reduce(arr, cb, start) {
+  let accum = start;
+  if(accum) {
+    for(let i=0; i<arr.length; i++) {
+      accum = cb(accum, arr[i])
+    }
+  } else {
+    accum = arr[0];
+    for(let i=1; i<arr.length; i++) {
+      accum = cb(accum, arr[i])
+    }
+  }
+  
+  return accum
+}

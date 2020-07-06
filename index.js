5b1e17f0a499a71456ef0e0fb27ef73bad9829ac
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-06 13:42:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(s,l){
+  let a = []
+
+for (let i = 0; i<s.length; i++){
+  let element = s[i]
+  a.push(l(element))
+}
+return a
+}
+function reduce(s,l,starting){
+  let r = (!!starting) ? starting : s[0]
+  let i = (!!starting) ? 0:1
+  for (; i < s.length; i++) {
+    r=l(s[i],r)
+  }
+  return r
+}

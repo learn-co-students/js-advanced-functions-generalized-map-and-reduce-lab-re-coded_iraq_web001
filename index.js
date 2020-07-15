27bdb023d3fcd43655e89dd3c205a8bd2c649af9
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-07-15 09:44:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+function map(src, callBack){
+  let nArr = []
+  src.forEach(val => nArr.push(callBack(val)))
+  return nArr
+}
+
+function reduce(src, callBack, SP){
+  let nVal = SP ? SP : src[0];
+  for (let i= SP ? 0 : 1; i<src.length; i++){
+    nVal = callBack(src[i], nVal)
+  }
+  return nVal
+}
